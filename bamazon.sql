b727ebe3cc252b0c0a2b3f1546a565e4866b0d10
Kwanhee <kwantangboby23@utexas.edu>
2018-05-20 17:27:35 -0500

    1st challenge

--- bamazon.sql
+++ bamazon.sql
@@ -4,7 +4,49 @@ CREATE DATABASE bamazon_DB;
 
 USE bamazon_DB;
 
-CREATE TABLE products (
+CREATE TABLE products(
+	id INTEGER(11) AUTO_INCREMENT NOT NULL primary key,
+    
+    ProductName VARCHAR(30) NOT NULL,
+    
+    Department VARCHAR(30) NOT NULL,
+    
+    Price INTEGER(11) NOT NULL,
+    
+    StockQuantity INTEGER(11) NOT NULL
+);
 
-)
 
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Pork", "Food", 10, 20);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Beef", "Food", 20, 10);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Chicken", "Food", 5, 40);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Turkey", "Food", 8, 25);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Coffee", "Drink", 2, 100);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Water", "Drink", 1, 200);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Juice", "Drink", 3, 80);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Tabasco", "Sauce", 4, 50);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("A1", "Sauce", 5, 45);
+
+INSERT INTO products(ProductName, Department, Price, StockQuantity)
+VALUE ("Note", "Office", 6, 33);
+
+
+
+SELECT * FROM bamazon_DB.products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,121 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+
+    port: 8889,
+
+
+    user: "root",
+
+
+    password: "root",
+    database: "bamazon_DB"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    readProducts();
+});
+
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("-----------------Welcome to Bamazon-----------------\n");
+        console.log("\n---------------------------------------------");
+        for (i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].id + "\nProduct name: " + res[i].ProductName + "\nDepartment: " + res[i].Department + "\nPrice: " + res[i].Price + "\nStockQuantity: " + res[i].StockQuantity + "\n---------------------------------------------");
+        }
+
+
+        start();
+    });
+}
+
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+
+
+
+        inquirer.prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "Which item you want to buy?? (Select id number)",
+                validate: function (value) {
+                    if (isNaN(value) === false
+                        && parseInt(value) > 0
+                        && parseInt(value) <= res.length) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How much do you want to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        ])
+            .then(function (answer) {
+                var wantToBuy = res[answer.id - 1];
+                var remainQuantity = wantToBuy.StockQuantity - answer.quantity;
+                if (wantToBuy.StockQuantity >= answer.quantity) {
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                StockQuantity: remainQuantity
+                            },
+                            {
+                                id: answer.id
+                            }
+                        ],
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log("Update" + "\n---------------------------------------------" + "\nID: " + wantToBuy.id + "\nPrpduct Name: " + wantToBuy.ProductName + "\nDepartment: " + wantToBuy.Department + "\nPrice: " + wantToBuy.Price + "\nStockQuantity: " + remainQuantity +
+                                "\n---------------------------------------------"
+                            );
+                            console.log("How many items have been sold?\n" + answer.quantity + " have been sold" + "\n---------------------------------------------");
+
+                            var totalCost = wantToBuy.Price * answer.quantity;
+
+                            console.log("The Total You Have Been Paid is :  $" + totalCost);
+
+
+                        }
+
+
+
+                    )
+                } else {
+                    console.log("We are sorry. We don't have enough stock\n");
+                    console.log("No changes have been made");
+                }
+                
+                connection.end();
+
+            });
+
+
+    });
+
+
+}
+
